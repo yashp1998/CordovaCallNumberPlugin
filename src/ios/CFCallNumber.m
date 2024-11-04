273fe18c0dbe2db08e88d00501d1febb76c156f0
Alejandro Dirgan <adirgan@gmail.com>
2024-11-04 19:38:07 -0300

    update to ios 18

--- src/ios/CFCallNumber.m
+++ src/ios/CFCallNumber.m
@@ -3,46 +3,32 @@
 
 @implementation CFCallNumber
 
-+ (BOOL)available {
-    return [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tel://"]];
-}
-
 - (void) callNumber:(CDVInvokedUrlCommand*)command {
-    
+
     [self.commandDelegate runInBackground:^{
-        
+
         __block CDVPluginResult* pluginResult = nil;
         NSString* number = [command.arguments objectAtIndex:0];
-        number = [number stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-        
-        if(![number hasPrefix:@"tel:"]){
+        number = [number stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+
+        if( ! [number hasPrefix:@"tel:"]){
             number =  [NSString stringWithFormat:@"tel:%@", number];
         }
 
-        // run in mainthread as below 
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if(![CFCallNumber available]) {
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NoFeatureCallSupported"];
-            }
-            else if(![[UIApplication sharedApplication] openURL:[NSURL URLWithString:number]]) {
-                // missing phone number
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"CouldNotCallPhoneNumber"];
-            } else {
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-            }
-        });
-        // return result
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        
-    }];
-}
+        NSURL* telURL = [NSURL URLWithString:number];
 
-- (void) isCallSupported:(CDVInvokedUrlCommand*)command {
-    [self.commandDelegate runInBackground: ^{
-        CDVPluginResult* pluginResult = [CDVPluginResult
-                                         resultWithStatus:CDVCommandStatus_OK
-                                         messageAsBool:[CFCallNumber available]];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        if (![[UIApplication sharedApplication] canOpenURL:telURL]) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NoFeatureCallSupported"];
+        } else {
+            [[UIApplication sharedApplication] openURL:telURL options:@{} completionHandler:^(BOOL success) {
+                if (success) {
+                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                } else {
+                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"CouldNotCallPhoneNumber"];
+                }
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            }];
+        }
     }];
 }
 
